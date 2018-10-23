111f5826a3d545f69248cd36e363451f779e42fb
eszterTari <tari.eszter.eng@gmail.com>
2018-10-23 16:29:28 +0200

    speed, nameNumber fields are implemented

--- src/com/codecool/Truck.java
+++ src/com/codecool/Truck.java
@@ -1,14 +1,17 @@
 package com.codecool;
 
+import java.util.Random;
+
 public class Truck {
 
     int speed = 100;
         // speed: 100km/h. 5% chance of breaking down for 2 hours.
-    int nameNumber;
+    Random rand = new Random();
+    int nameNumber = rand.nextInt(1001);
         // Truck drivers are boring. They call all their trucks a random number between 0 and 1000.
     int breakdownTurnsLeft;
         // holds how long its still broken down.
-    int distanceTraveled;
+    int distanceTraveled = 0;
     public void moveForAnHour(Race race) {
 
     }
