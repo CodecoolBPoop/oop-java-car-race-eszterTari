e2f46f8c294cc669b9ac262819931ee72d4481a1
eszterTari <tari.eszter.eng@gmail.com>
2018-10-23 16:02:01 +0200

    class' methods are implemented

--- src/com/codecool/Weather.java
+++ src/com/codecool/Weather.java
@@ -1,11 +1,21 @@
 package com.codecool;
 
+import java.util.Random;
+
 public class Weather {
 
-    public int static setRaining() {
+    public static int setRaining() {
         // 30% chance of rain.
+        Random calcChance = new Random();
+        int chance = calcChance.nextInt(11);
+        return chance;
     }
-    public static boolean isRaining() {
+
+    public static boolean isRaining(int chance) {
         // is it raining currently.
+        if (chance <= 3) {
+            return true;
+        }
+        return false;
     }
 }
