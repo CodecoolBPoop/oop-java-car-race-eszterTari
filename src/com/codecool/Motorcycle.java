a0a9f2f9688279057e9a5353521e197bdb807cd0
eszterTari <tari.eszter.eng@gmail.com>
2018-10-23 16:28:56 +0200

    speed options are implemented

--- src/com/codecool/Motorcycle.java
+++ src/com/codecool/Motorcycle.java
@@ -1,16 +1,20 @@
 package com.codecool;
 
+import java.util.Random;
+
 public class Motorcycle {
 
     int speed = 100;
+    Random speedRandom = new Random();
+    int speedRain = speed - (speedRandom.nextInt(46) + 5);
         // speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly).
-    public int static nameNumber() {
+    static int nameNumber;
         // The number of the instance created. Used for its name.
-    }
+
 
     String name;
         // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
-    int distanceTraveled;
+    int distanceTraveled = 0;
     public void moveForAnHour(Race race) {
 
     }
