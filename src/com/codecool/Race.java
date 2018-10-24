d7d6deab2a424cac70692e82dcb806989f66cbda
eszterTari <tari.eszter.eng@gmail.com>
2018-10-24 19:23:15 +0200

    methods are optimized for object maintaining; main() isn't finished yet

--- src/com/codecool/Race.java
+++ src/com/codecool/Race.java
@@ -1,59 +1,30 @@
 package com.codecool;
 
+import com.codecool.Vehicle;
+import java.util.ArrayList;
+
 public class Race {
 
-    static int carSpeed;
-    static int motorSpeed;
-    static int truckSpeed;
+    private ArrayList<Car> cars = new ArrayList<>();
+    private ArrayList<Motorcycle> motors = new ArrayList<>();
+    private ArrayList<Truck> trucks = new ArrayList<>();
+
     static int speedLimitWhenTruckBroken = 75;
-    static int carDistance;
-    static int motorDistance;
-    static int truckDistance;
     static boolean truckIsOkay = true;
-
-    public static void moveForAnHour() {
-        // Race race
-        // get the 3 vehicles' speed
-
-        motorSpeed = motorSpeedWhetherItsRaining();
-        carSpeed = Car.getCarSpeed();
-        if (truckIsOkay) {
-            truckSpeed = Truck.getTruckSpeed();
-        } else {
-            truckSpeed = 0;
-            truckIsOkay = true;
-        }
-
-        boolean isThereABrokenDownTruck = Truck.isItBrokenDown();
-        if (isThereABrokenDownTruck) {
-            truckSpeed = 0;
-            truckIsOkay = false;
-            carSpeed = speedLimitWhenTruckBroken;
-            if (motorSpeed > speedLimitWhenTruckBroken) {
-                motorSpeed = speedLimitWhenTruckBroken;
-            }
-        }
-
-        // calc the 3 vehicles' distance / 1 hour
-        carDistance = carDistance + carSpeed;
-        motorDistance = motorDistance + motorSpeed;
-        truckDistance = truckDistance + truckSpeed;
-    }
-
-    public static int motorSpeedWhetherItsRaining() {
-        int motorSpeedValue;
-        int chaneOfRain = Weather.setRaining();
-        boolean isItRaining = Weather.isRaining(chaneOfRain);
-        if (isItRaining) {
-            motorSpeedValue = Motorcycle.getSpeedRain();
-        } else {
-            motorSpeedValue = Motorcycle.getMotorspeed();
-        }
-        return motorSpeedValue;
-    }
 
     public void createVehicles() {
         // creates 10 cars, 10 trucks and 10 motorcycles.
+        for (int i = 0; i < 10; i++) {
+            cars.add(new Car());
+            motors.add(new Motorcycle());
+            trucks.add(new Truck());
+        }
+    }
+
+    public boolean isItRaining() {
+        int chanceOfRain = Weather.setRaining();
+        boolean raining = Weather.isRaining(chanceOfRain);
+        return raining;
     }
 
     public static void simulateRace() {
@@ -62,21 +33,31 @@ public class Race {
         // - setting whether its raining
         int i = 1;
         do {
+            System.out.println("Hour: " + i);
             moveForAnHour();
             i++;
         } while (i <= 50);
     }
 
-    public static void printRaceResults() {
+    public void printRaceResults() {
         // prints each vehicle's name, distance traveled ant type.
-        System.out.println("The car's distance:" + carDistance);
-        System.out.println("The motorcycle's distance:" + motorDistance);
-        System.out.println("The truck's distance:" + truckDistance);
+        for (Car car: cars) {
+            System.out.println("Car name: " + car.getName() +
+                    "Total distance traveled: " + car.getDistanceTraveled() + "km");
+        }
+        for (Motorcycle motor: motors) {
+            System.out.println("Motorcycle name: " + motor.getName() +
+                    "Total distance traveled: " + motor.getDistanceTraveled() + "km");
+        }
+        for (Truck truck: trucks) {
+            System.out.println("Truck name: " + truck.getName() +
+                    "Total distance traveled: " + truck.getDistanceTraveled() + "km");
+        }
     }
 
-/*    public boolean isThereABrokenTruck() {
-
-    }*/
+    public boolean isThereABrokenTruck() {
+        return false;
+    }
 
     public static void main(String[] args) {
 	// write your code here
