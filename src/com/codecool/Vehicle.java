722b9497b6311c652f0a0695fb2136e8e3030774
eszterTari <tari.eszter.eng@gmail.com>
2018-10-24 19:19:15 +0200

    new Vehicle class implemented (includes Car, Motorcycle, Truck classes)

---
+++ src/com/codecool/Vehicle.java
@@ -0,0 +1,26 @@
+package com.codecool;
+
+import com.codecool.Race;
+
+public class Vehicle {
+
+    String name;
+    int speed;
+    int distanceTraveled = 0;
+
+    public void moveForAnHour(Race race){
+        this.distanceTraveled += speed;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public Integer getDistanceTraveled() {
+        return distanceTraveled;
+    }
+}
