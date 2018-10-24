1b97c8c1bce0c513aafd159f89290967a12af013
eszterTari <tari.eszter.eng@gmail.com>
2018-10-24 19:21:36 +0200

    more understandable % calculating

--- src/com/codecool/Weather.java
+++ src/com/codecool/Weather.java
@@ -4,16 +4,18 @@ import java.util.Random;
 
 public class Weather extends Race {
 
+    public Weather(){}
+
     public static int setRaining() {
         // 30% chance of rain.
         Random calcChance = new Random();
-        int chance = calcChance.nextInt(11);
+        int chance = calcChance.nextInt(101);
         return chance;
     }
 
     public static boolean isRaining(int chance) {
         // is it raining currently.
-        if (chance <= 3) {
+        if (chance <= 30) {
             return true;
         }
         return false;
