defcbe34693ba5577b40594badbe8cd9905e6313
eszterTari <tari.eszter.eng@gmail.com>
2018-10-25 15:26:02 +0200

    class is refactored

--- src/com/codecool/Car.java
+++ src/com/codecool/Car.java
@@ -1,17 +1,15 @@
 package com.codecool;
 
 import java.util.Random;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
 
 public class Car extends Vehicle {
 
-    // Make a list from the words here: http://www.fantasynamegenerators.com/car-names.php
-    // and pick 2 randomly for each instance.
+    int speedLimitWhenTruckBroken = 75;
     static private String[] names = {"Scorpion", "Expedition", "Desire", "Barrage", "Scout",
             "Orbit", "Instinct", "Dragon", "Might", "Capital", "Bullet", "Twister", "Basilisk", "Eminance", "Expedition",
             "Dynamics", "Revelation", "Blast", "Paradox", "Tarragon"};
+        // Make a list from the words here: http://www.fantasynamegenerators.com/car-names.php
+        // and pick 2 randomly for each instance.
 
     public Car() {
         Random rand = new Random();
@@ -20,18 +18,18 @@ public class Car extends Vehicle {
             // the normal speed of the car. Set to a random number in the constructor between 80-110km/h.
     }
 
-    public void moveForAnHour(Race race) {
+    public void moveForAnHour(Race race, boolean isThereABrokenTruck) {
         // The vehicle travels for an hour. It increases the distance traveled. Call this from the main class only!
 
-        if (race.isThereABrokenTruck()) {
-            distanceTraveled += 75;
-            System.out.println(name + "traveled: 75 km in this hour");
+        if (isThereABrokenTruck) {
+            distanceTraveled += speedLimitWhenTruckBroken;
+            System.out.println(name + " travelled: 75 km in this hour because of a broken truck");
                 // If there is a broken down Truck on the track, then limit the max speed of vehicles to 75 km/h.
         } else {
             distanceTraveled += speed;
-            System.out.println(name + "traveled: "+ speed + "km in this hour");
+            System.out.println(name + " travelled: "+ speed + " km");
         }
-        System.out.println(name + "'s total distance traveled: " + distanceTraveled);
+        System.out.println(name + "'s total distance travelled: " + distanceTraveled + " km");
         System.out.println(" ");
     }
 
