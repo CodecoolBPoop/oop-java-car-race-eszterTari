54638a3cf319dd1407d7d648c3d9d088fb196e5f
eszterTari <tari.eszter.eng@gmail.com>
2018-10-25 15:25:28 +0200

    moveForAnHour() without bugs; class is refactored

--- src/com/codecool/Motorcycle.java
+++ src/com/codecool/Motorcycle.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 public class Motorcycle extends Vehicle {
 
-
     static int nameNumber = 1;
         // The number of the instance created. Used for its name.
+    int speedLimitWhenTruckBroken = 75;
 
     public Motorcycle() {
-        name = "Motorcycle" + nameNumber;
+        name = "Motorcycle " + nameNumber;
             // Are called "Motorcycle 1", "Motorcycle 2", "Motorcycle 3",... Unique.
         speed = 100;
         nameNumber++;
@@ -21,32 +21,33 @@ public class Motorcycle extends Vehicle {
         return speedInRain;
     }
 
-    public void moveForAnHour(Race race) {
-        if (race.isItRaining() && race.isThereABrokenTruck()) {
-            int tempSpeed = getSpeedRain(speed);
-            if (tempSpeed > 75) {
-                distanceTraveled += 75;
-                System.out.println(name + "traveled: 75 km in this hour because of a broken truck");
+    public void moveForAnHour(Race race, boolean raining, boolean isThereABrokenTruck) {
+        // speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly).
+        if (isThereABrokenTruck) {
+            if (raining) {
+                int speedRain = getSpeedRain(speed);
+                if (speedRain > speedLimitWhenTruckBroken) {
+                    distanceTraveled += speedLimitWhenTruckBroken;
+                    System.out.println(name + " travelled: 75 km in this hour because of a broken truck in the rain");
+                } else {
+                    distanceTraveled += speedRain;
+                    System.out.println(name + " travelled: " + speedRain + " km in this hour because of the rain (also there was a broken truck)");
+                }
             } else {
-                distanceTraveled += tempSpeed;
-                System.out.println(name + "traveled: " + tempSpeed + "km in this hour because of the rain");
-            }
-        }
-        else if (race.isItRaining()) {
-            // speed is 100km/h. If it rains, travels with 5-50km/h slower (randomly).
-            distanceTraveled += getSpeedRain(speed);
-            System.out.println(name + "traveled: " + speed + "km in this hour because of the rain");
-        }
-        else if (race.isThereABrokenTruck()) {
-            if (speed > 75) {
-                distanceTraveled += 75;
-                System.out.println(name + "traveled: 75 km in this hour because of a broken down truck");
+                distanceTraveled += speedLimitWhenTruckBroken;
+                System.out.println(name + " travelled: 75 km in this hour because of a broken truck");
             }
         } else {
-            distanceTraveled += speed;
-            System.out.println(name + "traveled: " + speed + "km in this hour");
+            if (raining) {
+                int speedInRain = getSpeedRain(speed);
+                distanceTraveled += speedLimitWhenTruckBroken;
+                System.out.println(name + " travelled: " + speedInRain + " km in this hour because of the rain");
+            } else {
+                distanceTraveled += speed;
+                System.out.println(name + " travelled: " + speed + " km");
+            }
         }
-        System.out.println(name + "'s total distance traveled: " + distanceTraveled);
+        System.out.println(name + "'s total distance travelled: " + distanceTraveled + " km");
         System.out.println(" ");
     }
 }
